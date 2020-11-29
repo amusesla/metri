ec7386b2d90393b19e701365186be349ebbc12fa
Amuse <amusesla@gmail.com>
2020-11-29 22:30:22 +0900

    Added: User model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    gender = models.CharField(max_length=100)
+    age = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.name
