63f66359ce26a223a85023de3c7b5726470d5e7f
Amuse <amusesla@gmail.com>
2020-11-29 22:10:07 +0900

    Initial Setting

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DATABASE 명',
+        'USER': 'root',
+        'PASSWORD': '비밀번호',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+SECRET = {
+        'secret':'시크릿키',
+}
