b54ac4feb9ea8e84c5aee0590f367797cfa373e8
Amuse <amusesla@gmail.com>
2020-11-29 23:05:14 +0900

    Added: product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'products'
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'brands'
+
+    def __str__(self):
+        return self.name
+
